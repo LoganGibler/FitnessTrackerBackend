06ee3f958acc40c52e3a11f0a60407455a0260c7
Logan Gibler <Logan.Gibler@gmail.com>
2021-10-27 18:50:37 -0400

    listens to port 3000

--- db/index.js
+++ db/index.js
@@ -1,1 +1,11 @@
 // require and re-export all files in this db directory (users, activities...)
+const { Client } = require("pg"); // imports the pg module
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+  });
+
+
+module.exports = {
+client
+}
--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // create the express server here
+const express = require("express");
+const { PORT = 3000} = process.env
+const server = express();
+const morgan = require("morgan");
+require("dotenv").config();
+server.use(morgan("dev"));
+server.use(express.json());
+
+server.use((req, res, next) => {
+    console.log("<____Body Logger START____>");
+    console.log(req.body);
+    console.log("<_____Body Logger END_____>");
+  
+    next();
+  });
+
+const { client } = require("./db");
+client.connect();
+
+server.listen(PORT, () => {
+    console.log("The server is up on port", PORT);
+  });
